4c1a3f2c2d173968cfa0709e2c0d22460b0af5c2
CamiloAOsorno333 <CamiloAOsorno333@gmail.com>
2024-06-24 11:08:37 -0500

    Se han subido dos archivos de operadores

---
+++ src/operadores/OperadoresAsignacion.java
@@ -0,0 +1,33 @@
+package operadores;
+
+public class OperadoresAsignacion {
+
+    public static void main(String[] args) {
+
+        int i= 5; //en este caso el = es el operador de asignación
+        int j= i + 4;
+        // combinados o compuestos
+
+        i = i +2;
+        System.out.println("i = " + i);
+
+        i += 5;
+        System.out.println("i = " + i);
+
+        i += 5;
+        System.out.println("i = " + i);
+        
+        j-= 4;
+        System.out.println("i = " + j);
+        
+        j*= 3;
+        System.out.println("j = " + j);
+
+        String sqlString= "selct * from clientes as c";
+
+        sqlString += "where c.nombre'Andres'";
+        sqlString += "and c.activos";
+        System.out.println("sqlString = " + sqlString);
+
+    }
+}
