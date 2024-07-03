4ebff3f5fafebb34c54d77b44262b330731d1ea1
CamiloAOsorno333 <CamiloAOsorno333@gmail.com>
2024-07-02 22:34:20 -0500

    se corrigio el desafio del nombre largo

--- src/desafios/ObtnerNombreMasLargo.java
+++ src/desafios/ObtnerNombreMasLargo.java
@@ -21,12 +21,12 @@ public class ObtnerNombreMasLargo {
 
     public static void main(String[] args) {
 
-        String persona1 = JOptionPane.showInputDialog("Ingresa el nombre y apellido de un familiar o amigo:");
-        String persona2 = JOptionPane.showInputDialog("Ingresa el nombre y apellido de un familiar o amigo:");
-        String persona3 = JOptionPane.showInputDialog("Ingresa el nombre y apellido de un familiar o amigo:");
+        String nombre1 = JOptionPane.showInputDialog("Ingresa el primer nombre por favor = ");
+        String nombre2 = JOptionPane.showInputDialog("Ingresa el segundo nombre por favor = ");
+        String nombre3 = JOptionPane.showInputDialog("Ingresa el tercer nombre por favor = ");
 
-        String max = (persona1.split(" ")[0].length() < persona2.split(" ")[0].length()) ? persona2 : persona1;
-        max = (persona3.split(" ")[0].length() < max.split(" ")[0].length()) ? max: persona3;
+        String max = (nombre1.split(" ")[0].length() < nombre2.split(" ")[0].length()) ? nombre2 : nombre1;
+        max = (nombre3.split(" ")[0].length() < max.split(" ")[0].length()) ? max: nombre3;
 
         System.out.println("La persona con el nombre mas largo es " + max);
     }
